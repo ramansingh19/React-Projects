3e51534059c67740de92fb02598be30a57487160
ramansingh19 <raman.01kumar1@gmail.com>
2025-09-20 19:09:59 +0530

    d

--- basic-react/src/button.jsx
+++ basic-react/src/button.jsx
@@ -9,14 +9,23 @@ function shivam(){
   
 }
 
+function doubleclick(){
+  console.log('You double clicked');
+  
+}
 
+function mouseover(){
+  console.log('you hover the mouse');
+  
+}
 
 function Button(){
   return (
     <div>
       <button onClick={print}>Click Me</button>
       <h1 onClick={shivam}>iPhone 16</h1>
-      
+      <button onDoubleClick={doubleclick}>Press Me</button>
+      <button onMouseMove={mouseover}>Click</button>
     </div>
   )
 }
