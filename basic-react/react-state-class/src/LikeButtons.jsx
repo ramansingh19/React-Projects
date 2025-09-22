da860f163add2e0e00a0e5fc9a1371f683446b6e
ramansingh19 <raman.01kumar1@gmail.com>
2025-09-22 07:50:15 +0530

    activity of click event in react

--- basic-react/react-state-class/src/LikeButtons.jsx
+++ basic-react/react-state-class/src/LikeButtons.jsx
@@ -1,14 +1,39 @@
 import { FaRegHeart } from "react-icons/fa";
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa6";
 
 export default function LikeButtons() {
-  let LikeButton = () => {
-    console.log('clicked');
+  let [isLiked , setisLiked] = useState(false)
+  let [clicks , setclicks] = useState(0)
+
+  let isToggle = () => {
+    setisLiked(!isLiked)
+    setclicks(clicks + 1)
+    document.body.style.backgroundColor = changeColor()
     
   }
 
+  let style = {color : 'red'}
+
+  let changeColor = () => {
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+    for(let i=1; i<=6; i++){
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+  };
+
+  
+
   return(
-    <div>
-      <button onClick={LikeButton}><FaRegHeart /></button>
+    <div >
+      <p>Clicks = {clicks}</p>
+      <button style={{fontSize : '3rem'}} onClick={isToggle} >
+        {isLiked ? 
+        (<FaHeart style={style} /> ) : 
+        (<FaRegHeart />)}
+        </button>
     </div>
   )
 }
