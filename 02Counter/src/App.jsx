c857a8f5f710e5d56d3283c27df7a6e307368381
ramansingh19 <raman.01kumar1@gmail.com>
2025-10-03 11:43:43 +0530

    about hooks , useState

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Counter'
+
+function App() {
+  return (
+    <>
+    <h1>Raman Singh</h1>
+    <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ 02Counter/src/Counter.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export default function Counterr() {
+
+let [counter , setCounter ] = useState(0)
+
+  // let counter = 5;
+
+  //increase the value 
+  const incCounter = () => {
+    if (counter >= 20) {
+      return
+    }else{
+      counter += 1;
+      setCounter(counter)
+      console.log(counter);
+    }
+    
+  }
+
+  //decrease the value
+
+  const decCounter = () => {
+    setCounter((prev) => {
+      if (prev <= 0) return 0;   // stop at 0
+      return prev - 1;
+    });
+  };
+
+
+  // const decCounter = () => {
+  //   if (counter < 1) {
+  //     return;
+  //   }else{
+  //     counter -= 1;
+  //     setCounter(counter)
+  //     console.log(counter);
+  //   }
+    
+  // }
+
+
+  return (
+    <>
+    <h1>Counter</h1>
+    <h2>Counter is : {counter}</h2>
+    <button onClick={incCounter}>Increase</button>
+    <br />
+    <button onClick={decCounter}>Decrease</button>
+    </>
+  )
+
+}
+
