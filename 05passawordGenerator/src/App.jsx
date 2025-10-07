c3b39560ea551216ddae64081939212a8e85004c
ramansingh19 <raman.01kumar1@gmail.com>
2025-10-07 19:31:45 +0530

    password changer

--- 05passawordGenerator/src/App.jsx
+++ 05passawordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 // import './App.css'
 
@@ -9,11 +9,15 @@ function App() {
   const [char , setChar] = useState(false)
   const [password , setpassword] = useState("")
 
+  //this is use for reference
+  const passwordRef = useRef(null)
+
+  //this is use for generating passwords
   const PasswordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(number) str += "0123456789"
-    if(char) str += "{}[]~`!@#$%^&*()_-+=?/<>"
+    if(number) str += "0123456789" //--> if you tap on the number section then numbers will be appear 
+    if(char) str += "{}[]~`!@#$%^&*()_-+=?/<>"//-->if you tap on the character section then character will be appear
     
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
@@ -24,6 +28,14 @@ function App() {
   
   }, [length , number , char , setpassword])
 
+  //this is use for copy the password
+  //usecallback is use for memoriziestion 
+  const copypasswordClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,4)
+      window.navigator.clipboard.writeText(password)
+  } ,[password])
+
   //if you insert password in the array your stuck into the infinite loop
   useEffect(() => {
       PasswordGenerator()
@@ -31,13 +43,19 @@ function App() {
   
   return (
     <>
-    
+
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 
       text-center my-10 text-orange-500 bg-gray-700'
       >Password Generator
         <div className=' text-black flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type='text' value={password} className=' bg-white  outline-none w-full py-2 px-3' placeholder='password ' readOnly/>
-        <button className='outline-none bg-blue-700 text-white px-3 py-1 shrink-0 '>Copy</button>
+        <input type='text' 
+        value={password} 
+        className=' bg-white  outline-none w-full py-2 px-3' placeholder='password ' 
+        readOnly
+        ref={passwordRef}
+        />
+        
+        <button onClick={copypasswordClipboard} className='outline-none bg-blue-700 text-white px-3 py-1 shrink-0 cursor-pointer'>Copy</button>
         </div>
 
           <div className='flex text-sm gap-x-2'>
