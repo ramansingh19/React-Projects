6e1ef76b1068ae142aa01e850a85b9e9f201296d
ramansingh19 <raman.01kumar1@gmail.com>
2025-10-16 11:40:20 +0530

    git commit -mss

--- 09ThemeChanger/src/App.jsx
+++ 09ThemeChanger/src/App.jsx
@@ -10,24 +10,26 @@ function App() {
   const lightMode = () => setThemeMode("light");
 
   useEffect(() => {
-  document.querySelector('html').classList.remove('light', 'dark')
-  document.querySelector('html').classList.add(themeMode)
-}, [themeMode])
+  document.documentElement.classList.remove("light", "dark");
+  document.documentElement.classList.add(themeMode);
+}, [themeMode]);
 
 
   return (
     <ThemeProvider value={{ themeMode, darkMode, lightMode }}>
-      <div className="flex flex-wrap min-h-screen items-center bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      
+
+      <div className="flex flex-wrap min-h-screen items-center bg-white  text-gray-900 dark:text-white">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
-
           <div className="w-full max-w-sm mx-auto">
             <Card />
           </div>
         </div>
       </div>
+      
     </ThemeProvider>
   );
 }
